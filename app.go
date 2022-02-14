1e568f60e8e9b51f7620d2fd1e6a5f382d89a91e
king <Administrator@jinghailun>
2022-02-14 17:24:59 +0800

    add file scan

--- app.go
+++ app.go
@@ -1,26 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"go-helper/file"
-	"os"
-)
-
 func main() {
-	osPath , _ := os.Getwd()
-	exist := file.FileIsExist(osPath+"/README.md")
-	if exist {
-		fmt.Println("file exist")
-	}else {
+	//osPath , _ := os.Getwd()
+	//exist := file.FileIsExist(osPath+"/README.md")
+	//if exist {
+	//	fmt.Println("file exist")
+	//}else {
+	//
+	//	fmt.Println("file not exist")
+	//}
+	//
+	//exist2,_ := file.FolderIsExist(osPath+"/file11", false)
+	//if exist2 {
+	//	fmt.Println("Folder Is Exist")
+	//}else {
+	//
+	//	fmt.Println("Folder Is Not Exist")
+	//}
 
-		fmt.Println("file not exist")
-	}
-
-	exist2,_ := file.FolderIsExist(osPath+"/file11", false)
-	if exist2 {
-		fmt.Println("Folder Is Exist")
-	}else {
-
-		fmt.Println("Folder Is Not Exist")
-	}
+	//ScanFile(osPath+"/file11")
 }
